04d965dc4aa5300599a8ac948e7132c6bf372922
LuzGuzman <124198612+LuzGuzman@users.noreply.github.com>
2025-09-01 20:32:07 -0600

    crea las funciones para mostrar la lista y limpiar la caja despues de un nombre

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaNombresSorteados=[];
+
+function mostrarLista() {
+    let ul = document.querySelector("#listaAmigos");
+    ul.innerHTML = ""; // Limpiar lista antes de volver a renderizar
+
+    listaNombresSorteados.forEach(nombre => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        ul.appendChild(li);
+    });
+}
+
+function limpiarCaja(){
+    document.querySelector("#amigo").value="";
+}
